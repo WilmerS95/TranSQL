0f7472265dd703adcf2b4baa74cb3db8f2d1b45a
Wilmer <wilmerantoniosinay@gmail.com>
2024-10-16 11:28:25 -0600

    Clases add

---
+++ TranSQL.shared/Reservacion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TranSQL.shared
+{
+    public class Reservacion
+    {
+        private int IdReservacion {  get; set; }
+
+        private DateTime FechaReservacion { get; set; }
+
+        private DateTime FechaInicio {  get; set; }
+
+        private DateTime FechaFin {  get; set; }
+
+        private string Observaciones {  get; set; } = string.Empty;
+        private int IdSolicitud { get; set; }
+
+        private string Placa { get; set; } = string.Empty;
+
+        private SolicitudReservacion solicitudReservacion;
+        private Vehiculo vehiculo;
+
+        public Reservacion(int idReservacion, DateTime fechaReservacion, DateTime fechaInicio, DateTime fechaFin, string observaciones, int idSolicitud, string placa, SolicitudReservacion solicitudReservacion, Vehiculo vehiculo)
+        {
+            IdReservacion = idReservacion;
+            FechaReservacion = fechaReservacion;
+            FechaInicio = fechaInicio;
+            FechaFin = fechaFin;
+            Observaciones = observaciones ?? throw new ArgumentNullException(nameof(observaciones));
+            IdSolicitud = idSolicitud;
+            Placa = placa ?? throw new ArgumentNullException(nameof(placa));
+            this.solicitudReservacion = solicitudReservacion ?? throw new ArgumentNullException(nameof(solicitudReservacion));
+            this.vehiculo = vehiculo ?? throw new ArgumentNullException(nameof(vehiculo));
+        }
+
+        public Reservacion()
+        {
+        }
+    }
+}
---
+++ TranSQL.shared/SolicitudReservacion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranSQL.shared
+{
+    public class SolicitudReservacion
+    {
+        private int IdSolicitud {  get; set; }
+
+        [Required]
+        [StringLength(300)]
+        private string Motivo { get; set; } = string.Empty;
+
+        [Required]
+        private DateTime Fecha { get; set; }
+
+        private int IdColaborador { get; set; }
+        private int IdEstadoSolicitud { get; set; }
+        private string JustificacionRechazo { get; set; } = string.Empty;
+
+        private Colaborador colaborador;
+        private EstadoSolicitud estadoSolicitud;
+
+        public SolicitudReservacion(int idSolicitud, string motivo, DateTime fecha, int idColaborador, int idEstadoSolicitud, string justificacionRechazo, Colaborador colaborador, EstadoSolicitud estadoSolicitud)
+        {
+            IdSolicitud = idSolicitud;
+            Motivo = motivo ?? throw new ArgumentNullException(nameof(motivo));
+            this.Fecha = fecha;
+            this.IdColaborador = idColaborador;
+            this.IdEstadoSolicitud = idEstadoSolicitud;
+            this.JustificacionRechazo = justificacionRechazo ?? throw new ArgumentNullException(nameof(justificacionRechazo));
+            this.colaborador = colaborador ?? throw new ArgumentNullException(nameof(colaborador));
+            this.estadoSolicitud = estadoSolicitud ?? throw new ArgumentNullException(nameof(estadoSolicitud));
+        }
+
+        public SolicitudReservacion()
+        {
+        }
+    }
+}
