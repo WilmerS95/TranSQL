3a594dedea65b172590dc6482f4d231b4dfb01b2
Wilmer <wilmerantoniosinay@gmail.com>
2024-10-21 12:51:35 -0600

    Se configuran los controllers para cada clase con sus respectivos CRUD

---
+++ TranSQL.server/Controllers/InspeccionAccesoriosController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TranSQL.shared;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TranSQL.server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InspeccionAccesoriosController : ControllerBase
+    {
+        private readonly TranSQLDbContext _context;
+
+        public InspeccionAccesoriosController(TranSQLDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<InspeccionAccesoriosController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InspeccionAccesorio>>> GetInspeccionAccesorios()
+        {
+            return await _context.InspeccionesAccesorios.ToListAsync();
+        }
+
+        // GET api/<InspeccionAccesoriosController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<InspeccionAccesorio>> GetInspeccionAccesorio(int id)
+        {
+            var inspeccionAccesorio = await _context.InspeccionesAccesorios.FindAsync(id);
+            if (inspeccionAccesorio == null)
+            {
+                return NotFound();
+            }
+
+            //return Ok(inspeccionAccesorio);
+            return inspeccionAccesorio;
+        }
+
+        // POST api/<InspeccionAccesoriosController>
+        [HttpPost]
+        public async Task<ActionResult<InspeccionAccesorio>> PostInspeccionAccesorio(InspeccionAccesorio inspeccionAccesorio)
+        {
+            _context.InspeccionesAccesorios.Add(inspeccionAccesorio);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetInspeccionAccesorio", new { id = inspeccionAccesorio.IdInspeccion }, inspeccionAccesorio);
+
+        }
+
+        // PUT api/<InspeccionAccesoriosController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutInspeccionAccesorio(int id, InspeccionAccesorio inspeccionAccesorio)
+        {
+            if (id != inspeccionAccesorio.IdInspeccion)
+            {
+                return BadRequest();
+            }
+            _context.Entry(inspeccionAccesorio).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // DELETE api/<InspeccionAccesoriosController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteInspeccionAccesorio(int id)
+        {
+            var inspeccionAccesorio = await _context.InspeccionesAccesorios.FindAsync(id);
+            if (inspeccionAccesorio == null)
+            {
+                return NotFound();
+            }
+            _context.InspeccionesAccesorios.Remove(inspeccionAccesorio);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ TranSQL.server/Controllers/TipoVehiculosController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TranSQL.shared;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TranSQL.server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoVehiculosController : ControllerBase
+    {
+        private readonly TranSQLDbContext _context;
+
+        public TipoVehiculosController(TranSQLDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<TipoVehiculosController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TipoVehiculo>>> GetTipoVehiculos()
+        {
+            return await _context.TipoVehiculos.ToListAsync();
+        }
+
+        // GET api/<TipoVehiculosController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TipoVehiculo>> GetTipoVehiculo(int id)
+        {
+            var tipoVehiculos = await _context.TipoVehiculos.FindAsync(id);
+            if (tipoVehiculos == null)
+            {
+                return NotFound();
+            }
+
+            //return Ok(tipoVehiculos);
+            return tipoVehiculos;
+        }
+
+        // POST api/<TipoVehiculosController>
+        [HttpPost]
+        public async Task<ActionResult<TipoVehiculo>> PostTipoVehiculo(TipoVehiculo tipoVehiculo)
+        {
+            _context.TipoVehiculos.Add(tipoVehiculo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTipoVehiculo", new { id = tipoVehiculo.IdTipoVehiculo }, tipoVehiculo);
+
+        }
+
+        // PUT api/<TipoVehiculosController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTipoVehiculo(int id, TipoVehiculo tipoVehiculo)
+        {
+            if (id != tipoVehiculo.IdTipoVehiculo)
+            {
+                return BadRequest();
+            }
+            _context.Entry(tipoVehiculo).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // DELETE api/<TipoVehiculosController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTipoVehiculo(int id)
+        {
+            var tipoVehiculo = await _context.TipoVehiculos.FindAsync(id);
+            if (tipoVehiculo == null)
+            {
+                return NotFound();
+            }
+            _context.TipoVehiculos.Remove(tipoVehiculo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
