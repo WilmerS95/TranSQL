e5025ab729825975908ef7fa8c0438223d4b7897
Wilmer <wilmerantoniosinay@gmail.com>
2024-10-23 20:14:26 -0600

    Se configuran cors

--- TranSQL.server/Program.cs
+++ TranSQL.server/Program.cs
@@ -6,30 +6,34 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-// Configuración de CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigins",
-    policy =>
-    {
-        policy.WithOrigins(
-            "http://localhost:5020", "https://localhost:5020", "https://localhost:7223", "https://localhost:7221", "http://localhost:7223", "http://localhost:5018")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<TranSQLDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("cadenaSQL")));
+
+builder.Services.AddDbContext<TranSQLDbContext>(opciones  =>
+{
+    opciones.UseSqlServer(builder.Configuration.GetConnectionString("cadenaSQL"));
+});
+
+// Configuración de CORS
+var corsPolicyName = "DefaultCorsPolicy";
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsPolicyName, policyBuilder =>
+    {
+        policyBuilder.WithOrigins(allowedOrigins)
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
 // Aplicar CORS
-app.UseCors("AllowSpecificOrigins");
+app.UseCors(corsPolicyName);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
