cf47ab6962c9bfd4f39db35dfdb262e3a39eb0f3
donis <donis@LAPTOP-KHFTDDCQ>
2024-10-23 21:16:54 -0600

    login

---
+++ TranSQL.server/Controllers/AccesorioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TranSQL.server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccesorioController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AccesorioController (IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public async Task <IActionResult> GetAccesorio()
+        {
+            string query = @"SELECT * FROM Accesorio";
+            DataTable table 
+
+        }
+    }
+}
